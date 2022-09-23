ea2c652531cacc1c0e848490de4d73383ca4d63f
Angelo-Netho <angelonetho@gmail.com>
2022-09-22 23:45:29 -0300

    Implements update function

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -4,6 +4,7 @@ import { HttpContext } from '@adonisjs/core/build/standalone'
 import BadRequestException from 'App/Exceptions/BadRequestException'
 import User from 'App/Models/User'
 import CreateUserValidator from 'App/Validators/CreateUserValidator'
+import UpdateUserValidator from 'App/Validators/UpdateUserValidator'
 
 export default class UsersController {
   public async store({ request, response }: HttpContext) {
@@ -15,5 +16,21 @@ export default class UsersController {
 
     const user = await User.create(userPayLoad)
     return response.created({ user })
+  }
+
+  public async update({ request, response }: HttpContext) {
+    const { email, name } = await request.validate(UpdateUserValidator)
+    const id = request.param('id')
+    const user = await User.findOrFail(id)
+
+    const userByEmail = await User.findBy('email', email)
+
+    if (userByEmail) throw new BadRequestException('email already in use', 409)
+
+    user.email = email
+    user.name = name
+    await user.save()
+
+    return response.ok({ user })
   }
 }
