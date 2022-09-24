466e2b368657ed88556e874bf7e48e644a367ca6
Angelo-Netho <angelonetho@gmail.com>
2022-09-24 20:49:23 -0300

    feat: Now the token is generated

--- app/Controllers/Http/PasswordsController.ts
+++ app/Controllers/Http/PasswordsController.ts
@@ -2,6 +2,8 @@ import Mail from '@ioc:Adonis/Addons/Mail'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 import ForgotPasswordValidator from 'App/Validators/ForgotPasswordValidator'
+import { randomBytes } from 'crypto'
+import { promisify } from 'util'
 
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
@@ -10,6 +12,18 @@ export default class PasswordsController {
     const { email, resetPasswordUrl } = await request.validate(ForgotPasswordValidator)
     const user = await User.findByOrFail('email', email)
 
+    const random = await promisify(randomBytes)(24)
+    const token = random.toString('hex')
+    await user.related('tokens').updateOrCreate(
+      {
+        userId: user.id,
+      },
+      {
+        token,
+      }
+    )
+
+    const resetPasswordUrlWithToken = `${resetPasswordUrl}?token=${token}`
     await Mail.send((message) => {
       message
         .from('no-reply@ifprinteligente.com.br')
@@ -18,7 +32,7 @@ export default class PasswordsController {
         .htmlView('email/forgotpassword', {
           productName: 'Smart Harpia',
           name: user.name,
-          resetPasswordUrl: 'url',
+          resetPasswordUrl: resetPasswordUrlWithToken,
         })
     })
     return response.noContent()
