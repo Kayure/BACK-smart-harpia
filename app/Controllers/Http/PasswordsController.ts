2a4f85eee283e96ffe5f5a983d2a6cf17a2c5ac9
Angelo-Netho <angelonetho@gmail.com>
2022-09-24 18:16:25 -0300

    Creates Password Controller

---
+++ app/Controllers/Http/PasswordsController.ts
@@ -0,0 +1,7 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class PasswordsController {
+  public async forgotPassword({}: HttpContextContract) {}
+}
