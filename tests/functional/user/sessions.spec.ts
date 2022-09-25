72303c3165bcab95406ea766a29c1dd2d898fe46
Angelo-Netho <angelonetho@gmail.com>
2022-09-25 18:37:26 -0300

    Creating sessions test

---
+++ tests/functional/user/sessions.spec.ts
@@ -0,0 +1,22 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import { test } from '@japa/runner'
+
+import { UserFactory } from './../../../database/factories/index'
+
+test.group('Session', (group) => {
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+    return () => Database.rollbackGlobalTransaction()
+  })
+
+  test('it should authenticate an user', async ({ client, assert }) => {
+    const plainPassword = 'test'
+    const { id, email } = await UserFactory.merge({ password: plainPassword }).create()
+    const response = await client.post('/sessions').form({ email, password: plainPassword })
+    const body = response.body()
+
+    response.assertStatus(201)
+    assert.isDefined(body.user, 'User undefined')
+    assert.equal(body.user.id, id)
+  })
+})
