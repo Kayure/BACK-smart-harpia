2696253ecb32e13abd2664dd59cddaddbe24e16f
Angelo-Netho <angelonetho@gmail.com>
2022-09-25 18:53:25 -0300

    Testing error scenarios

--- tests/functional/user/sessions.spec.ts
+++ tests/functional/user/sessions.spec.ts
@@ -31,4 +31,28 @@ test.group('Session', (group) => {
     assert.isDefined(body.token, 'Token undefined')
     assert.equal(body.user.id, id)
   })
+
+  test('it should return 400 when credentials are not provided', async ({ client, assert }) => {
+    const response = await client.post('/sessions').form({})
+    const body = response.body()
+
+    response.assertStatus(400)
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 400)
+    assert.equal(body.message, 'invalid credentials')
+  })
+
+  test('it should return 400 when credentials are invalid', async ({ client, assert }) => {
+    const { email } = await UserFactory.create()
+    const response = await client.post('/sessions').form({
+      email,
+      password: 'test',
+    })
+    const body = response.body()
+
+    response.assertStatus(400)
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 400)
+    assert.equal(body.message, 'invalid credentials')
+  })
 })
