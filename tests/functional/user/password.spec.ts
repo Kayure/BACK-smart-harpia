0337025775fe3c2447c2362a8be5859908ed0074
Angelo-Netho <angelonetho@gmail.com>
2022-09-24 21:24:39 -0300

    Create password reset tests

--- tests/functional/user/password.spec.ts
+++ tests/functional/user/password.spec.ts
@@ -1,3 +1,5 @@
+import { DateTime, Duration } from 'luxon'
+import Hash from '@ioc:Adonis/Core/Hash'
 import Mail from '@ioc:Adonis/Addons/Mail'
 import Database from '@ioc:Adonis/Lucid/Database'
 import { test } from '@japa/runner'
@@ -47,7 +49,77 @@ test.group('Forgot Password', (group) => {
   }).timeout(0)
 
   test('it should return 422 when required data is not provided', async ({ client, assert }) => {
-    const response = await client.post('/users').form({})
+    const response = await client.post('/forgot-password').form({})
+    const body = response.body()
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
+  })
+})
+
+test.group('Reset Password', (group) => {
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+    return () => Database.rollbackGlobalTransaction()
+  })
+
+  test('it should be able to reset password', async ({ client, assert }) => {
+    const user = await UserFactory.create()
+    const { token } = await user.related('tokens').create({ token: 'token' })
+
+    const response = await client.post('/reset-password').form({
+      token,
+      password: '123456',
+    })
+
+    await user.refresh()
+    await Hash
+
+    response.assertStatus(204)
+    const checkPassword = await Hash.verify(user.password, '123456')
+    assert.isTrue(checkPassword)
+  })
+
+  test('it should return 404 when using the same token twice', async ({ client, assert }) => {
+    const user = await UserFactory.create()
+    const { token } = await user.related('tokens').create({ token: 'token' })
+
+    await client.post('/reset-password').form({
+      token,
+      password: '123456',
+    })
+
+    const response = await client.post('/reset-password').form({
+      token,
+      password: '123456',
+    })
+    const body = response.body()
+
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 404)
+  })
+
+  test('it cannot reset password when token is expired after 2 hours', async ({
+    client,
+    assert,
+  }) => {
+    const user = await UserFactory.create()
+    const date = DateTime.now().minus(Duration.fromISOTime('02:01'))
+    const { token } = await user.related('tokens').create({ token: 'token', createdAt: date })
+
+    const response = await client.post('/reset-password').form({
+      token,
+      password: '123456',
+    })
+    const body = response.body()
+
+    response.assertStatus(410)
+    assert.equal(body.code, 'TOKEN_EXPIRED')
+    assert.equal(body.status, 410)
+    assert.equal(body.message, 'token has expired')
+  })
+
+  test('it should return 422 when required data is not provided', async ({ client, assert }) => {
+    const response = await client.post('/reset-password').form({})
     const body = response.body()
     assert.equal(body.code, 'BAD_REQUEST')
     assert.equal(body.status, 422)
