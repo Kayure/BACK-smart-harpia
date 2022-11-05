665702d89547d57909995120e38cb6e710f42adf
Angelo-Netho <angelonetho@gmail.com>
2022-11-05 18:59:31 -0300

    Create Log controller

---
+++ app/Controllers/Http/LogsController.ts
@@ -0,0 +1,54 @@
+import { HttpContext } from '@adonisjs/core/build/standalone'
+import Device from 'App/Models/Device'
+import Log from 'App/Models/Log'
+import CreateLogValidator from 'App/Validators/CreateLogValidator'
+import UpdateLogValidator from 'App/Validators/UpdateLogValidator'
+import { DateTime } from 'luxon'
+
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class LogsController {
+  public async store({ request, response }: HttpContext) {
+    const logPayLoad = await request.validate(CreateLogValidator)
+
+    let device = await Device.query()
+      .where('macAddress', logPayLoad.macAddress)
+      .andWhere('mdevId', logPayLoad.mdevId)
+      .first()
+
+    if (device === null) {
+      device = await Device.create({
+        mdevId: logPayLoad.mdevId,
+        macAddress: logPayLoad.macAddress,
+      })
+    }
+
+    const log = await Log.create({
+      deviceId: device!.id,
+    })
+
+    await log.related('device').associate(device!)
+
+    return response.created({ log })
+  }
+
+  public async update({ request, response }: HttpContext) {
+    const logPayLoad = await request.validate(UpdateLogValidator)
+
+    const device = await Device.query()
+      .where('macAddress', logPayLoad.macAddress)
+      .andWhere('mdevId', logPayLoad.mdevId)
+      .firstOrFail()
+
+    const log = await Log.query()
+      .where('deviceId', device.id)
+      .andHavingNull('leavedAt')
+      .firstOrFail()
+
+    log.leavedAt = DateTime.now()
+
+    log.save()
+
+    return response.ok({ log })
+  }
+}
