712acb6dfc2cd2279e8e2ee93754d154cdb174dc
Angelo-Netho <angelonetho@gmail.com>
2022-11-05 23:00:41 -0300

    Create Instituition update, read adn destroy functions

--- app/Controllers/Http/InstituitionsController.ts
+++ app/Controllers/Http/InstituitionsController.ts
@@ -1,6 +1,8 @@
 import { HttpContext } from '@adonisjs/core/build/standalone'
 import Instituition from 'App/Models/Instituition'
 import CreateInstituitionValidator from 'App/Validators/CreateInstituitionValidator'
+import UpdateInstituitionValidator from 'App/Validators/UpdateInstituitionValidator'
+
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class InstituitionsController {
@@ -9,5 +11,34 @@ export default class InstituitionsController {
 
     const instituition = await Instituition.create(instituitionPayLoad)
     return response.created({ instituition })
+  }
+
+  public async update({ request, response }: HttpContext) {
+    const { name, abbreviation } = await request.validate(UpdateInstituitionValidator)
+    const id = request.param('id')
+
+    const instituition = await Instituition.findOrFail(id)
+
+    instituition.name = name
+    instituition.abbreviation = abbreviation
+
+    instituition.save()
+
+    return response.ok({ instituition })
+  }
+
+  public async destroy({ request, response }: HttpContext) {
+    const id = request.param('id')
+
+    const instituition = await Instituition.findOrFail(id)
+    instituition.delete()
+
+    return response.noContent()
+  }
+
+  public async read({ response }: HttpContext) {
+    const instituitions = await Instituition.all()
+
+    return response.ok({ instituitions })
   }
 }
