688d63bbfcd53142dfca684960adc447925408c9
Angelo-Netho <angelonetho@gmail.com>
2022-11-05 23:12:12 -0300

    Create occupation update, read adn destroy functions

--- app/Controllers/Http/OccupationsController.ts
+++ app/Controllers/Http/OccupationsController.ts
@@ -1,6 +1,7 @@
 import { HttpContext } from '@adonisjs/core/build/standalone'
 import Occupation from 'App/Models/Occupation'
 import CreateOccupationValidator from 'App/Validators/CreateOccupationValidator'
+import UpdateOccupationValidator from 'App/Validators/UpdateOccupationValidator'
 
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
@@ -10,5 +11,34 @@ export default class OccupationsController {
 
     const occupation = await Occupation.create(occupationPayLoad)
     return response.created({ occupation })
+  }
+
+  public async update({ request, response }: HttpContext) {
+    const { name, description } = await request.validate(UpdateOccupationValidator)
+    const id = request.param('id')
+
+    const occupation = await Occupation.findOrFail(id)
+
+    occupation.name = name
+    occupation.description = description
+
+    occupation.save()
+
+    return response.ok({ occupation })
+  }
+
+  public async destroy({ request, response }: HttpContext) {
+    const id = request.param('id')
+
+    const occupation = await Occupation.findOrFail(id)
+    occupation.delete()
+
+    return response.noContent()
+  }
+
+  public async read({ response }: HttpContext) {
+    const occupations = await Occupation.all()
+
+    return response.ok({ occupations })
   }
 }
