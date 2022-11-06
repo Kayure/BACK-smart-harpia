e95b2b2c07c8e735b092e236bbc43732bdb32d34
Angelo-Netho <angelonetho@gmail.com>
2022-11-05 21:11:56 -0300

    Refactor Log migration to add mdev_id

--- database/migrations/1667272330933_logs.ts
+++ database/migrations/1667272330933_logs.ts
@@ -7,6 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.integer('device_id').unsigned().references('devices.id').onDelete('CASCADE')
+      table.integer('mdev_id').unsigned().references('mdevs.id').onDelete('CASCADE')
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
